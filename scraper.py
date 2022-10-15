7606b6b56964f09fbe9475b459d6d49e31347394
Luka Cabraja <luka@cabraja.eu>
2022-10-15 18:27:22 +0200

    implemented synchronous async and working diff

--- scraper.py
+++ scraper.py
@@ -4,6 +4,8 @@ from email.mime import base
 from re import sub
 import requests, json, os
 from dotenv import load_dotenv
+import asyncio
+from tqdm.asyncio import tqdm
 
 ##            _               
 ##           | |              
@@ -13,6 +15,20 @@ from dotenv import load_dotenv
 ##  |___/\___|\__|\__,_| .__/ 
 ##                     | |    
 ##                     |_|    
+
+def get_last_data(path):
+    data = {}
+    if not os.path.exists(path): 
+        open(path, 'x').close()
+    else:
+        fp = open(path, 'r')
+        data = "".join(fp.readlines())
+        try:
+            data = json.loads("data_string")
+        except:
+            data = {}
+            print(f"Error loading last session from file: [{path.split('/')[-1]}]")
+    return data
 
 
 load_dotenv()
@@ -27,22 +43,13 @@ print("  | | _ __ | |_ ___ | |__  __| |_   _| | ____ _    | /  \/| |     | |  ")
 print("  | || '_ \|  _/ _ \|  __|/ _` | | | | |/ / _` |   | |    | |     | |  ")
 print(" _| || | | | || (_) | |__| (_| | |_| |   < (_| |   | \__/\| |_____| |_ ")
 print(" \___/_| |_|_| \___/\____/\__,_|\__,_|_|\_\__,_|    \____/\_____/\___/ ")
+print()
+print()
 
 if savefile is None: savefile = ".infoeduka.json"
 if outdir is None: outdir = "files"
 
-# if not os.path.exists(savefile): 
-#     open(savefile, 'x').close()
-#     data = {}
-# else:
-#     fp = open(savefile, 'r')
-#     data_string = "".join(fp.readlines())
-#     try:
-#         data = json.loads("data_string")
-#     except:
-#         data = {}
-
-base_url="https://student.racunarstvo.hr"
+last_data = get_last_data(savefile)
 
 ##    __                  _   _                 
 ##   / _|                | | (_)                
@@ -51,6 +58,10 @@ base_url="https://student.racunarstvo.hr"
 ##  | | | |_| | | | | (__| |_| | (_) | | | \__ \
 ##  |_|  \__,_|_| |_|\___|\__|_|\___/|_| |_|___/
 ##  
+
+def save_data(path, data):
+    fp = open(path, 'w')
+    fp.write(json.dumps(data))
 
 def post_login(login_username, login_password):
     url = "https://student.racunarstvo.hr/digitalnareferada/api/login"
@@ -111,48 +122,74 @@ def parse_materials(materials_response):
                 }
 
                 for category in subject["dodatno"]["materijali"]["kategorije"]:
-                    materials_data[subject["idPredmet"]]["materials"]["files"][category["kategorija"]] = []
+                    materials_data[subject["idPredmet"]]["materials"]["files"][category["kategorija"]] = {}
                     for material in category["materijali"]:
-                        materials_data[subject["idPredmet"]]["materials"]["files"][category["kategorija"]].append({
-                            "id": material["id"],
+                        materials_data[subject["idPredmet"]]["materials"]["files"][category["kategorija"]][material["id"]] = {
                             "filename": material["naziv"],
                             "description": material["opis"],
                             "bytes": material["velicina"],
                             "date_uploaded": material["vrijeme"],
                             "user_uploaded": material["korisnika"],
                             "content_type": material["contentType"],
-                            "url": material["link"]
-                        })
+                            "url": material["link"],
+                            "downloaded": False,
+
+                            "subject_name": subject["predmet"],
+                            "subject_code": subject["sifra"],
+                            "subject_id": subject["idPredmet"],
+                            "mat_category": material["kategorija"]
+                        }
     return materials_data
 
-def download(session_token, path, file):
+def materials_diff(old_materials_data, new_materials_data):
+    if len(old_materials_data.keys()) > 0:
+        for id_course, course in new_materials_data.items():
+            if id_course in old_materials_data.keys() and course["materials"]["last_changed_date"] == old_materials_data[id_course]["materials"]["last_changed_date"]:
+                for category, files in course["materials"]["files"].items():
+                    for id_file, file in files.items():
+                        if category in old_materials_data[id_course]["materials"]["files"] and \
+                            id_file in old_materials_data[id_course]["materials"]["files"][category] and \
+                            old_materials_data[id_course]["materials"]["files"][category][id_file]["downloaded"] == True:
+                            new_materials_data[id_course]["materials"]["files"][category][id_file]["downloaded"] = True
+
+def category_path_from_file(file):
+    basepath = os.path.join(os.getcwd(), os.path.join(outdir, file["subject_name"].replace("/", "-")))
+    category_path = os.path.join(basepath, file["mat_category"].replace("/", "-"))
+    return category_path
+
+async def download(session_token, file, index):
     baseurl = "https://student.racunarstvo.hr/digitalnareferada/"
     url = os.path.join(baseurl, file["url"])
     
+    path = category_path_from_file(file)
     filepath = os.path.join(path, file["filename"])
     copy_index = 1;
     while os.path.exists(filepath):
         filepath = os.path.join(path, file["filename"] + f"_{copy_index}")
         copy_index += 1
-    print(filepath)
     headers = {"Cookie": f"PHPSESSID={session_token}"}
-    response = requests.request("GET", url, headers=headers)
-    
-    with open(filepath, mode='wb') as response_file:
-        response_file.write(response.content)
-    
-    return True
 
-def download_materials(session_token, materials_data, semester_filter):
-    files = {}
+    response = requests.get(url, headers=headers, stream=True)
+    with tqdm.wrapattr(open(os.devnull, "wb"), "write", 
+            miniters=1, desc=file["filename"],
+            total=int(response.headers.get('content-length', 0))
+    ) as fout:
+        for chunk in response.iter_content(chunk_size=4096):
+            fout.write(chunk)
+
+async def download_materials(session_token, materials_data, semester_filter):
+    queued_files = []
     for _, subject in materials_data.items():
         basepath = os.path.join(os.getcwd(), os.path.join(outdir, subject["name"].replace("/", "-")))
         if ('|'.join([subject["academic_year"], subject["semester"]]) == semester_filter):
             for category, files in subject["materials"]["files"].items():
                 category_path = os.path.join(basepath, category.replace("/", "-"))
                 if not os.path.exists(category_path): os.makedirs(category_path)
-                for file in files:
-                    download(session_token, category_path, file)
+                for id, file in files.items():
+                    if not file["downloaded"]:
+                        queued_files.append((file))
+    ret = await asyncio.gather(*[download(session_token, file, index) for index, file in enumerate(queued_files)])
+    print("DONE!")
 
 ##                 _       _          _             _   
 ##                (_)     | |        | |           | |  
@@ -165,8 +202,9 @@ def download_materials(session_token, materials_data, semester_filter):
 
 login_response_data, session_id = post_login(username, password)
 materials_response_data = parse_materials(get_materials(session_id))
+materials_diff(last_data, materials_response_data)
 semester_filter = "2022/2023|Zimski"
-download_materials(session_id, materials_response_data, semester_filter)
+asyncio.run(download_materials(session_id, materials_response_data, semester_filter))
 
 ##        _                              
 ##       | |                             
@@ -177,5 +215,5 @@ download_materials(session_id, materials_response_data, semester_filter)
 ##                                | |    
 ##                                |_|    
 
-# fp = open(savefile, 'w')
-# fp.write(json.dumps(data))
+print(json.dumps(materials_response_data, indent=4, ensure_ascii=False))
+save_data(savefile, materials_response_data)
